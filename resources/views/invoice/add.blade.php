64fd5bb4c431ef06972bee4fe2447e3436166062
kritikaOddeven <kritikaoddeveninfotech@gmail.com>
2025-08-06 17:37:06 +0530

    create invoice

--- resources/views/invoice/add.blade.php
+++ resources/views/invoice/add.blade.php
@@ -45,40 +45,6 @@
                                 </div>
 
                                 <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-fullname">Category Name</label>
-                                    <select class="form-select mb-4" name="cat_id">
-                                        <option value="Jordan Stevenson">Jordan Stevenson</option>
-                                        <option value="Wesley Burland">Wesley Burland</option>
-                                        <option value="Vladamir Koschek">Vladamir Koschek</option>
-                                        <option value="Tyne Widmore">Tyne Widmore</option>
-                                    </select>
-                                    @error('name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-fullname">SUb-Category Name</label>
-                                    <select class="form-select mb-4" name="subcat_id">
-                                        <option value="Jordan Stevenson">Jordan Stevenson</option>
-                                        <option value="Wesley Burland">Wesley Burland</option>
-                                        <option value="Vladamir Koschek">Vladamir Koschek</option>
-                                        <option value="Tyne Widmore">Tyne Widmore</option>
-                                    </select>
-                                    @error('name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-fullname"> Name</label>
-                                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="John Doe" />
-                                    @error('name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
                                     <label class="form-label" for="basic-default-phone">Bill Number</label>
                                     <input type="text" name="bill_no" class="form-control phone-mask @error('bill_no') is-invalid @enderror" value="{{ old('bill_no') }}" placeholder="658 799 8941" />
                                     @error('bill_no')
@@ -94,92 +60,49 @@
                                     @enderror
                                 </div>
 
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-phone">HSN Code</label>
-                                    <input type="text" name="hsncode" class="form-control phone-mask @error('hsncode') is-invalid @enderror" value="{{ old('hsncode') }}" placeholder="658 799 8941" />
-                                    @error('hsncode')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
+
 
                                 <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-phone">Number </label>
-                                    <input type="text" name="number" class="form-control phone-mask @error('number') is-invalid @enderror" value="{{ old('number') }}" placeholder="658 799 8941" />
-                                    @error('number')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-phone">Feet</label>
-                                    <input type="text" name="feet" class="form-control phone-mask @error('feet') is-invalid @enderror" value="{{ old('feet') }}" placeholder="GST Number" />
-                                    @error('feet')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-company">Feet Word</label>
-                                    <input type="text" class="form-control @error('feet_word') is-invalid @enderror" name="feet_word" value="{{ old('feet_word') }}" placeholder="Gandhinagar" />
-                                    @error('feet_word')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                 <div class="col-md-3 mb-3 mt-4">
                                     <div class="form-check form-switch mb-2">
-                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-                                        <label class="form-check-label" for="flexSwitchCheckDefault">GST</label>
+                                    <label class="form-label" for="basic-default-phone">Type</label>
+
+                                        <input type="checkbox" name="without_gst" value="0"><span>Without GST</span>
+                                        <input type="checkbox" name="gst" value="1"><span>GST</span>
+
                                     </div>
                                 </div>
 
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-phone">CGST </label>
-                                    <input type="text" name="cgst" class="form-control phone-mask @error('cgst') is-invalid @enderror" value="{{ old('cgst') }}" placeholder="658 799 8941" />
-                                    @error('cgst')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
+                                <div class="table-responsive text-nowrap">
+                                    <table class="table">
+                                        <thead class="table-secondary">
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>HSN Code</th>
+                                                <th>Number</th>
+                                                <th>Feet</th>
+                                                <th>Feet Word</th>
+                                                <th>Single Price</th>
+                                                <th>Total</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="table-border-bottom-0">
+                                            <tr>
+                                                <td>Albert Cook</td>
+                                                <td>
+                                                    486458675
+                                                </td>
+                                                <td>4</td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>100</td>
+                                                <td>400</td>
+
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
                                 </div>
 
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-phone">SGST</label>
-                                    <input type="text" name="sgst" class="form-control phone-mask @error('sgst') is-invalid @enderror" value="{{ old('sgst') }}" placeholder="GST Number" />
-                                    @error('sgst')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-3 mb-3">
-                                    <label class="form-label" for="basic-default-company">Estimated Total</label>
-                                    <input type="text" class="form-control @error('estimated_total') is-invalid @enderror" name="estimated_total" value="{{ old('estimated_total') }}" placeholder="Gandhinagar" />
-                                    @error('estimated_total')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-company">Single Price</label>
-                                    <input type="text" class="form-control @error('single_price') is-invalid @enderror" name="single_price" value="{{ old('single_price') }}" placeholder="Gandhinagar" />
-                                    @error('single_price')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-company">Total Price</label>
-                                    <input type="text" class="form-control @error('total_price') is-invalid @enderror" name="total_price" value="{{ old('total_price') }}" placeholder="Gandhinagar" />
-                                    @error('total_price')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label class="form-label" for="basic-default-company">Total </label>
-                                    <input type="text" class="form-control @error('total') is-invalid @enderror" name="total" value="{{ old('total') }}" placeholder="Gandhinagar" />
-                                    @error('total')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
 
                             </div>
                             <div class="text-end">
